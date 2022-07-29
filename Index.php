208aab5c1bd995f000542a0141da190c31c590d5
Lucas Silva <lucas.silva_1994@outlook.com>
2022-07-28 21:31:29 -0300

    Adicionando atributos e o método sacar a classe Conta, e criando objetos a partir da classe conta.

--- Index.php
+++ Index.php
@@ -1,2 +1,19 @@
 <?php
 
+require_once('src/Conta.php');
+
+$primeiraConta = new Conta();
+$primeiraConta->nomeTitular = "Pessoa 01";
+$primeiraConta->cpfTitular = "539.332.150-30";
+$primeiraConta->saldo = 200;
+
+$segundaConta = new Conta();
+$segundaConta->nomeTitular = "Pessoa 02";
+$segundaConta->cpfTitular = "123.814.290-77";
+$segundaConta->saldo = 400;
+
+$primeiraConta->sacar(198);
+$segundaConta->sacar(404);
+print "<pre>";
+var_dump([$primeiraConta,$segundaConta]);
+print "</pre>";
--- src/Conta.php
+++ src/Conta.php
@@ -1,5 +1,15 @@
 <?php
 
 class Conta{
-	
+	public string $cpfTitular;
+	public string $nomeTitular;
+	public float $saldo;
+
+	public function sacar(float $valorASacar): void{
+		if ($valorASacar > $this->saldo) 
+			print "Saldo indisponível <br/>";
+		 else
+			$this->saldo -= $valorASacar;
+		
+	}
 }
